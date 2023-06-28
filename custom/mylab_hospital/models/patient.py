8e87c9ea51cd9b6d0d3e83a1f1edcabc597f52b0
Ruben-JR <rubenpina758@gmail.com>
2023-06-28 12:03:31 -0100

    tracking fields

--- custom/mylab_hospital/models/patient.py
+++ custom/mylab_hospital/models/patient.py
@@ -9,7 +9,7 @@ class hospitalPatient(models.Model):
     _description = "Hospital Patient"
 
     ref = fields.Char(readonly=True)
-    name = fields.Char()
-    age = fields.Integer()
-    gender = fields.Selection([("m", "Male"), ("f", "Female")])
+    name = fields.Char(tracking=True, required=True)
+    age = fields.Integer(tracking=True)
+    gender = fields.Selection([("m", "Male"), ("f", "Female")], tracking=True)
     active = fields.Boolean(default=True)

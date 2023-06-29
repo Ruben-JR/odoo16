bd600ed389218d3d58184b098e0092b71349432e
Ruben-JR <rubenpina758@gmail.com>
2023-06-29 15:08:49 -0100

    default, onchange, depends

--- custom/mylab_hospital/models/appointment.py
+++ custom/mylab_hospital/models/appointment.py
@@ -9,5 +9,12 @@ class hospitalAppointment(models.Model):
     _description = "Hospital Appointment"
 
     patient_id = fields.Many2one("hospital.patient")
-    appoint_time = fields.Datetime()
-    book_date = fields.Date()
+    ref = fields.Char(readonly=True, related="patient_id.ref")
+    gender = fields.Selection(related="patient_id.gender")
+    appoint_time = fields.Datetime(default=fields.Datetime.now)
+    book_date = fields.Date(default=fields.Date.context_today)
+
+    @api.onchange("patient_id")
+    def _onchange_patient_id(self):
+        for rec in self:
+            rec.ref = rec.patient_id.ref
--- custom/mylab_hospital/models/patient.py
+++ custom/mylab_hospital/models/patient.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import date
 
 
 class hospitalPatient(models.Model):
@@ -8,8 +9,27 @@ class hospitalPatient(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Hospital Patient"
 
-    ref = fields.Char(readonly=True)
+    ref = fields.Char()
     name = fields.Char(tracking=True, required=True)
-    age = fields.Integer(tracking=True)
-    gender = fields.Selection([("m", "Male"), ("f", "Female")], tracking=True)
+    date_of_birth = fields.Date()
+    age = fields.Integer(compute="_calc_age", tracking=True)
+    gender = fields.Selection(
+        [("m", "Male"), ("f", "Female")], tracking=True, default="m"
+    )
     active = fields.Boolean(default=True)
+
+    @api.depends("date_of_birth")
+    def _calc_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = (
+                    today.year
+                    - rec.date_of_birth.year
+                    - (
+                        (today.month, today.day)
+                        < (rec.date_of_birth.month, rec.date_of_birth.day)
+                    )
+                )
+            else:
+                rec.age = 0
